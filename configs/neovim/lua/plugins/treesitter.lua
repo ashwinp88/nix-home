e98e82fa66e88a68d9e17b245b55be478d3a109b
Ashwin Purushotaman <ashwin.p88@gmail.com>
2025-10-10 20:45:51 -0400

    treesitter guard on first install

--- configs/neovim/lua/plugins/treesitter.lua
+++ configs/neovim/lua/plugins/treesitter.lua
@@ -2,7 +2,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      vim.notify("nvim-treesitter not available", vim.log.levels.WARN)
+      return
+    end
+
+    configs.setup({
       ensure_installed = {
         "lua",
         "vim",
